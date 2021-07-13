4a0f7a078b3026fd34b0790cb50dd2367c86eb60
Bryce <80635028+vargob27@users.noreply.github.com>
2021-07-13 17:35:06 -0400

    updated

--- 02-java-oop/01-project-class/Project.java
+++ 02-java-oop/01-project-class/Project.java
@@ -1,25 +1,42 @@
 public class Project {
     private String name;
     private String description;
-    public Project() {
-
-    }
-
-    public Project(String name) {
-        this.name = name;
-        // System.out.println("Class name: " + this.getClass().getSimpleName());
-
-    }
+    private static String defaultName = "Default Name";
+    private static String defaultDescription = "Default Description";
+    private double initialCost = 0;
 
     public void setName(String name) {
         this.name = name;
         // System.out.println("Class name: " + this.getClass().getSimpleName());
         // System.out.println("Project name: " + name);
     }
-
     public void setDescription(String desc) {
         this.description = desc;
         // System.out.println(this.name + " " + this.description);
+    }
+    public void setInitialCost(double price) {
+        this.initialCost = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public double getCost() {
+        return initialCost;
+    }
+    
+    public Project() {
+        this.name = defaultName;
+        this.description = defaultDescription;
+    }
+
+    public Project(String name) {
+        this.name = name;
+        this.description = defaultDescription;
+        // System.out.println("Class name: " + this.getClass().getSimpleName());
     }
 
     public Project(String name, String description) {
@@ -28,8 +45,13 @@ public class Project {
 
     }
 
+    public Project(String name, double price, String description) {
+        this.name = name;
+        this.initialCost = price;
+        this.description = description;
+    }
+
     public String elevatorPitch() {
-        String pitch = this.name + ": " + this.description;
-        return pitch;
+        return String.format("%s (%.02f): %s", name, initialCost, description);
     }
 }
