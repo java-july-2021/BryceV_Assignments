90949f8c2c44d6ee5157b9b0271503efc5d2c063
Bryce <80635028+vargob27@users.noreply.github.com>
2021-07-14 16:17:39 -0400

    assignment complete

---
+++ 02-java-oop/03-zoo-keeper-pt1/Gorilla.java
@@ -0,0 +1,17 @@
+public class Gorilla extends Mammal {
+	public Gorilla(int energyLevel) {
+        super(energyLevel);
+	}
+	public void throwSomething() {
+		System.out.println(this.getClass().getSimpleName() + " is throwing something here");
+		this.energyLevel -= 5;
+	}
+	public void eatBanana() {
+		System.out.println(this.getClass().getSimpleName() + " is eating bananas");
+		this.energyLevel += 10;
+	}
+	public void climb() {
+		System.out.println(this.getClass().getSimpleName() + " is climbing a tree");
+		this.energyLevel -= 10;
+	}
+}
---
+++ 02-java-oop/03-zoo-keeper-pt1/Tester.java
@@ -0,0 +1,12 @@
+public class Tester {
+    public static void main(String[] args) {
+        Gorilla george = new Gorilla(100);
+        george.throwSomething();
+        george.throwSomething();
+        george.throwSomething();
+        george.eatBanana();
+        george.eatBanana();
+        george.climb();
+        george.displayEnergy();
+    }
+}
